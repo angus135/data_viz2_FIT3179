9772d290f679d4267fb58b2db1a227c85748f999
Angus Corr Desktop <angus.corr@monash.edu>
2025-10-15 20:00:33 +1100

    updating path

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,24 +1,24 @@
-var vg_1 = "../vega-lite/renewable_map.json";
+var vg_1 = "vega-lite/renewable_map.json";
 vegaEmbed("#renewable_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "../vega-lite/renewable_state_breakdown.json";
+var vg_2 = "vega-lite/renewable_state_breakdown.json";
 vegaEmbed("#renewable_state_breakdown", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "../vega-lite/energy_production_over_time.json";
+var vg_3 = "vega-lite/energy_production_over_time.json";
 vegaEmbed("#energy_production_over_time", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_4 = "../vega-lite/project_time_bubble.json";
+var vg_4 = "vega-lite/project_time_bubble.json";
 vegaEmbed("#project_time_bubble", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_5 = "../vega-lite/capacity_tree_map.json";
+var vg_5 = "vega-lite/capacity_tree_map.json";
 vegaEmbed("#capacity_tree_map", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
